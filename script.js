802ac8ea8497c5e05addde0e70504fd8d185703d
Magva7 <magva7@gmail.com>
2020-05-25 11:00:27 +0300

    Добавил блок вывода - пока умножает значение первого ползунка на 20

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function testToConsole(){
     var polzunok1Var = document.getElementById("polzun1").value; //сохраняем в переменную значение первого ползунка
+    polzunok1Var = polzunok1Var*20;
     document.getElementById('outputSpan').innerHTML = polzunok1Var; //выводим в элементке с id outputSpan переменную polzunok1Var
     // console.log("Значение ползунка: "+polzunok1Var); //тестовый вывод в консоль
 
