75d0129e574577c58cddcb771e6a13f21eb67a8b
Magva7 <magva7@gmail.com>
2020-05-25 11:37:29 +0300

    Добавил третий ползунок, но в расчет его пока не включал

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function testToConsole(){
+function calcAndOutput(){
     var polzunok1Var = document.getElementById("polzun1").value; //сохраняем в переменную значение первого ползунка
     var polzunok2Var = document.getElementById("polzun2").value;
     var outputVar = polzunok1Var*polzunok2Var;
-    document.getElementById('outputSpan').innerHTML = outputVar; //выводим в элементке с id outputSpan переменную polzunok1Var
+    document.getElementById('outputAllMoney').innerHTML = outputVar; //выводим в элементке с id outputSpan переменную polzunok1Var
     // console.log("Значение ползунка: "+polzunok1Var); //тестовый вывод в консоль
 
     
