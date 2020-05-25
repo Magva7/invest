452936d0db58f1a29891209008b6dc6495dccd8c
Magva7 <magva7@gmail.com>
2020-05-25 10:28:11 +0300

    Сохранил значение первого ползунка в переменную - сохранение работает при изменении ползунка и для отладки пишется в консоль

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function testToConsole(){
-    console.log("Ползунок изменился!");
+    var polzunok1 = document.getElementById("polzun1").value;
+    // var polzunok1 = document.querySelector('.polzunok1'); //записываем в переменную значение ползунка
+    // console.log("Ползунок изменился!");
+    // console.log("Значение ползунка: "+polzunok1.value);
+    console.log("Значение ползунка: "+polzunok1);
 }
+
+
+
+// var email = document.querySelector('.subscription-email');
+// message.textContent = email.value;
+
+// onchange="document.getElementById('rangeValue').innerHTML = this.value*2; 
