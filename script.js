f65e936d92a8b39d9e079e99249b593a44da91a3
Magva7 <magva7@gmail.com>
2020-05-25 17:11:21 +0300

    Похоже, что теперь правильно считает, ну или близко к правде

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 function calcAndOutput(){
-    // var startMoneyVar = document.getElementById("startMoney").value; //сохраняем в переменную значение первого ползунка
-    // var addMoneyVar = document.getElementById("addMoney").value;
-    // var yearsVar = document.getElementById("years").value;
-    // var percentVar = document.getElementById("percent").value;
+    var startMoneyVar = document.getElementById("startMoney").value; //сохраняем в переменную значение первого ползунка
+    var addMoneyVar = document.getElementById("addMoney").value;
+    var srokVar = document.getElementById("srok").value;
+    var percentVar = document.getElementById("percent").value;
     
-    // // Смотрим, что в переменных до цикла
-    // console.log ("В startMoneyVar до цикла: "+startMoneyVar);
+    // Смотрим, что в переменных до цикла
+    console.log ("В startMoneyVar до цикла: "+startMoneyVar);
+    console.log ("В addMoneyVar до цикла: "+addMoneyVar);
+    console.log ("В srokVar до цикла: "+srokVar);
+    console.log ("В percentVar до цикла: "+percentVar);
 
-    // var outputVar = startMoneyVar;
+    var outputVar = startMoneyVar*1000;
+    // outputVar = outputVar*1000 + outputVar*(percentVar/100)/12+addMoneyVar*1000;
+    // outputVar = Math.round(outputVar);
+    // console.log ("В outputVar до цикла: "+outputVar);
+    
 
-    // for (var i=0; i<5; i++){
-    //     console.log(outputVar);
-    //     outputVar += outputVar*percentVar/12+addMoneyVar;
-    //     console.log ("В startMoneyVar в цикле: "+startMoneyVar);
-    // }
+    for (var i=0; i<srokVar*12; i++){
+        console.log(outputVar);
+        outputVar = outputVar + outputVar*(percentVar/100)/12+addMoneyVar*1000;
+        outputVar = Math.round(outputVar);
+        // console.log ("В startMoneyVar в цикле: "+startMoneyVar);
+        console.log("Итерация: "+i)
+    }
+    outputVar = Math.round(outputVar);
 
+    document.getElementById('outputAllMoney').innerHTML = outputVar; //выводим в элементке с id outputSpan переменную outputAllMoney
+    // // console.log("Значение ползунка: "+polzunok1Var); //тестовый вывод в консоль
 
-    // document.getElementById('outputAllMoney').innerHTML = outputVar; //выводим в элементке с id outputSpan переменную polzunok1Var
-    // console.log("Значение ползунка: "+polzunok1Var); //тестовый вывод в консоль
-
-    // Тестовые переменные
+    
 
     
 }
 
-var iznachalnyeBabki = 80000;
-    var plusVMesyac = 10000;
-    var procent = 0.05;
-    var srok = 24;
+// Тестовые переменные
+// var iznachalnyeBabki = 80000;
+//     var plusVMesyac = 10000;
+//     var procent = 0.05;
+//     var srok = 24;
 
-    var itogo = iznachalnyeBabki;
-    // console.log(itogo);
+//     var itogo = iznachalnyeBabki;
+//     // console.log(itogo);
 
-    for (var i=0; i<srok;i++){
-        itogo = itogo + itogo*procent/12+plusVMesyac;
-        // itogo = Math.round(itogo);
-        // console.log(itogo);
-    }
-    itogo = Math.round(itogo);
-    console.log(itogo);
+//     for (var i=0; i<srok;i++){
+//         itogo = itogo + itogo*procent/12+plusVMesyac;
+//         // itogo = Math.round(itogo);
+//         // console.log(itogo);
+//     }
+//     itogo = Math.round(itogo);
+//     console.log(itogo);
