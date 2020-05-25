f9dd0e6256fe9a0bb6738708bbfff7543d88e43f
Magva7 <magva7@gmail.com>
2020-05-25 16:45:35 +0300

    Рабочий вариант с тестовыми переменными

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 function calcAndOutput(){
-    var startMoneyVar = document.getElementById("startMoney").value; //сохраняем в переменную значение первого ползунка
-    var addMoneyVar = document.getElementById("addMoney").value;
-    var yearsVar = document.getElementById("years").value;
-    var percentVar = document.getElementById("percent").value;
+    // var startMoneyVar = document.getElementById("startMoney").value; //сохраняем в переменную значение первого ползунка
+    // var addMoneyVar = document.getElementById("addMoney").value;
+    // var yearsVar = document.getElementById("years").value;
+    // var percentVar = document.getElementById("percent").value;
     
-    // Смотрим, что в переменных до цикла
-    console.log ("В startMoneyVar до цикла: "+startMoneyVar);
+    // // Смотрим, что в переменных до цикла
+    // console.log ("В startMoneyVar до цикла: "+startMoneyVar);
 
-    var outputVar = startMoneyVar;
+    // var outputVar = startMoneyVar;
 
-    for (var i=0; i<5; i++){
-        console.log(outputVar);
-        outputVar += outputVar*percentVar/12+addMoneyVar;
-        console.log ("В startMoneyVar в цикле: "+startMoneyVar);
-    }
+    // for (var i=0; i<5; i++){
+    //     console.log(outputVar);
+    //     outputVar += outputVar*percentVar/12+addMoneyVar;
+    //     console.log ("В startMoneyVar в цикле: "+startMoneyVar);
+    // }
 
 
     // document.getElementById('outputAllMoney').innerHTML = outputVar; //выводим в элементке с id outputSpan переменную polzunok1Var
     // console.log("Значение ползунка: "+polzunok1Var); //тестовый вывод в консоль
 
-    
+    // Тестовые переменные
 
+    
 }
+
+var iznachalnyeBabki = 80000;
+    var plusVMesyac = 10000;
+    var procent = 0.05;
+    var srok = 24;
+
+    var itogo = iznachalnyeBabki;
+    // console.log(itogo);
+
+    for (var i=0; i<srok;i++){
+        itogo = itogo + itogo*procent/12+plusVMesyac;
+        // itogo = Math.round(itogo);
+        // console.log(itogo);
+    }
+    itogo = Math.round(itogo);
+    console.log(itogo);
