a2856c19fa87bba15f1be9a445659d36315d4e85
Magva7 <magva7@gmail.com>
2020-05-25 12:44:03 +0300

    В общем расчеты пока не работают

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function calcAndOutput(){
-    var polzunok1Var = document.getElementById("polzun1").value; //сохраняем в переменную значение первого ползунка
-    var polzunok2Var = document.getElementById("polzun2").value;
-    var outputVar = polzunok1Var*polzunok2Var;
-    document.getElementById('outputAllMoney').innerHTML = outputVar; //выводим в элементке с id outputSpan переменную polzunok1Var
+    var startMoneyVar = document.getElementById("startMoney").value; //сохраняем в переменную значение первого ползунка
+    var addMoneyVar = document.getElementById("addMoney").value;
+    var yearsVar = document.getElementById("years").value;
+    var percentVar = document.getElementById("percent").value;
+    
+    // Смотрим, что в переменных до цикла
+    console.log ("В startMoneyVar до цикла: "+startMoneyVar);
+
+    var outputVar = startMoneyVar;
+
+    for (var i=0; i<5; i++){
+        console.log(outputVar);
+        outputVar += outputVar*percentVar/12+addMoneyVar;
+        console.log ("В startMoneyVar в цикле: "+startMoneyVar);
+    }
+
+
+    // document.getElementById('outputAllMoney').innerHTML = outputVar; //выводим в элементке с id outputSpan переменную polzunok1Var
     // console.log("Значение ползунка: "+polzunok1Var); //тестовый вывод в консоль
 
     
